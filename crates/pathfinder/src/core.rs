af7cb94701d87dc44fcb7519af9974cfd7dcbbef
Krzysztof Lis <klis33@gmail.com>
2022-07-21 14:34:52 +0200

    feat: add derive[Hash] to StorageAddress

--- crates/pathfinder/src/core.rs
+++ crates/pathfinder/src/core.rs
@@ -93,7 +93,7 @@ pub struct CallSignatureElem(pub StarkHash);
 pub struct ByteCodeWord(pub StarkHash);
 
 /// The address of a storage element for a StarkNet contract.
-#[derive(Copy, Clone, PartialEq, Eq, Deserialize, Serialize, PartialOrd, Ord)]
+#[derive(Copy, Clone, PartialEq, Eq, Deserialize, Serialize, PartialOrd, Ord, Hash)]
 pub struct StorageAddress(pub StarkHash);
 
 /// The value of a storage element for a StarkNet contract.
